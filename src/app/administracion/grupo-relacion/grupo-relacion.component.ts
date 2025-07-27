86ef110c898a3a8048ecf9cff383d8b0448ae128
camilo9090 <camilou9090@gmail.com>
2025-07-27 13:24:27 -0500

    Agregar funcionalidad para gestionar programas y espacios académicos en el componente Grupo Relación.

--- src/app/administracion/grupo-relacion/grupo-relacion.component.ts
+++ src/app/administracion/grupo-relacion/grupo-relacion.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { GrupoRelacion, IGrupoRelacion } from '../../model/grupo-relacion';
-import { Grupo } from '../../model/grupo-model';
 import { Facultad } from '../../model/facultad-model';
 import { Sede } from '../../model/sede-model';
 import { EspacioAcademico } from '../../model/espacio-academico';
@@ -14,6 +13,9 @@ import { EspacioAcademicoService } from '../../services/espacio-academico.servic
 import { FacultadService } from '../../services/facultad.service';
 import { GrupoRelacionService } from '../../services/grupo-relacion.service';
 import Swal from 'sweetalert2';
+import { EspacioPrograma } from '../../model/espacio-programa';
+import { Programa } from '../../model/programa-model';
+import { EspacioProgramaService } from '../../services/espacio-programa.service';
 
 @Component({
   selector: 'app-grupo-relacion',
@@ -22,110 +24,82 @@ import Swal from 'sweetalert2';
   providers: [MessageService],
 })
 export class GrupoRelacionComponent implements OnInit {
-
   listGruposRelacion: IGrupoRelacion[] = [];
-  listGrupos: Grupo[] = [];
   listSedes: Sede[] = [];
   listEspacios: EspacioAcademico[] = [];
+  listProgramas: Programa[] = [];
   listFacultades: Facultad[] = [];
+  listEspaciosPrograma: EspacioPrograma[] = [];
+  espaciosFiltrados: EspacioAcademico[] = [];
   grupoRelacion?: IGrupoRelacion;
 
   newGrupoRelacion: GrupoRelacion = new GrupoRelacion();
 
-  grupoSeleccionado?: Grupo;
   sedeSeleccionada?: Sede;
-  espacioSeleccionado?: EspacioAcademico;
   facultadSeleccionada?: Facultad;
-
+/*   espacioAcademicoSeleccionado?: EspacioAcademico;
+  programaSeleccionado?: Programa; */
 
   displayCrearGrupo: boolean = false;
   displayEditarGrupo: boolean = false;
 
-  filteredOptions?: any[];
-  selectedOption: any = null;
+  /* filteredOptions?: any[]; */
+  /*  selectedOption: any = null; */
 
-  noDocumento?: string
+  noDocumento?: string;
 
   mensaje = Utilities;
 
-
   fg = new FormGroup({
-
-    grupo: new FormControl('', [Validators.required]),
     sede: new FormControl('', [Validators.required]),
     facultad: new FormControl('', [Validators.required]),
+    programa: new FormControl('', [Validators.required]),
     espacio: new FormControl('', [Validators.required]),
-  })
+  });
 
   constructor(
-    private grupoRelacionService: GrupoRelacionService,
-    private grupoService: GrupoService,
+    private grupoRelacionService: GrupoRelacionService,  
     private sedeService: SedeService,
-    private espacioService: EspacioAcademicoService,
+    private espacioProgramaService: EspacioProgramaService,
     private facultadService: FacultadService,
-    private messageService: MessageService,
-  ) { }
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.listarGruposRelacion();
-    this.listarGrupos();
     this.listarSedes();
-    this.listarEspaciosAcademicos();
     this.listarFacultades();
-    this.noDocumento = window.sessionStorage.getItem(UtilConstants.NUM_IDENTIFICACION)!;
-  }
+    this.listarEspaciosPrograma();
 
-  GrupoSelecconado(event: any) {
-    console.log("El grupo seleccionado es: ", event);
-
-    if (event) {
-      this.grupoSeleccionado = this.listGrupos.find((grupo) => grupo.idGrupo === event)!;
-
-    } else {
-      this.grupoSeleccionado = undefined;
-    }
+    this.noDocumento = window.sessionStorage.getItem(
+      UtilConstants.NUM_IDENTIFICACION
+    )!;
   }
 
   SedeSelecconado(event: any) {
-    console.log("El grupo seleccionado es: ", event);
+    console.log('El grupo seleccionado es: ', event);
 
     if (event) {
-      this.sedeSeleccionada = this.listSedes.find((sede) => sede.idSede === event)!;
-
+      this.sedeSeleccionada = this.listSedes.find(
+        (sede) => sede.idSede === event
+      )!;
     } else {
       this.sedeSeleccionada = undefined;
     }
   }
-  EspacioSelecconado(event: any) {
-    console.log("El grupo seleccionado es: ", event);
 
-    if (event) {
-      this.espacioSeleccionado = this.listEspacios.find((espacio) => espacio.idEspacioAcademico === event)!;
-
-    } else {
-      this.espacioSeleccionado = undefined;
-    }
-  }
   FacultadSelecconado(event: any) {
-    console.log("El grupo seleccionado es: ", event);
+    console.log('El grupo seleccionado es: ', event);
 
     if (event) {
-      this.facultadSeleccionada = this.listFacultades.find((facultad) => facultad.idFacultad === event)!;
-
+      this.facultadSeleccionada = this.listFacultades.find(
+        (facultad) => facultad.idFacultad === event
+      )!;
     } else {
       this.facultadSeleccionada = undefined;
     }
   }
-  //----- Metodos que permiten listar 
-
-  listarGrupos() {
-    this.grupoService.listarGrupos().subscribe({
-      next: (datagrupo) => {
-        this.listGrupos = datagrupo;
-      },
-      error: (dataerror) => console.log(dataerror),
-    });
-  }
+  //----- Metodos que permiten listar
 
   listarSedes() {
     this.sedeService.listarSedes().subscribe({
@@ -135,14 +109,7 @@ export class GrupoRelacionComponent implements OnInit {
       error: (dataerror) => console.log(dataerror),
     });
   }
-  listarEspaciosAcademicos() {
-    this.espacioService.listarEspaciosAcademicos().subscribe({
-      next: (dataespacio) => {
-        this.listEspacios = dataespacio;
-      },
-      error: (dataerror) => console.log(dataerror),
-    });
-  }
+
   listarFacultades() {
     this.facultadService.listarFacultades().subscribe({
       next: (datafacultad) => {
@@ -157,10 +124,54 @@ export class GrupoRelacionComponent implements OnInit {
       next: (datagruporelacion) => {
         this.listGruposRelacion = datagruporelacion;
       },
-      error: (dataerror) => console.error('Eror al listar los grupos relacion', dataerror),
+      error: (dataerror) =>
+        console.error('Eror al listar los grupos relacion', dataerror),
     });
+  }
 
+  listarEspaciosPrograma() {
+    this.listEspaciosPrograma = [];
+    this.espacioProgramaService.listarEspacioPrograma().subscribe({
+      next: (data) => {
+        this.listEspaciosPrograma = data;
 
+        // Extraer programas únicos
+        this.listProgramas = Array.from(
+          new Map(
+            data.map((item) => [item.programa?.idPrograma, item.programa])
+          ).values()
+        ).filter((p): p is Programa => p !== undefined);
+
+        // Extraer espacios académicos únicos
+        this.listEspacios = Array.from(
+          new Map(
+            data.map((item) => [
+              item.espacioAcademico?.idEspacioAcademico,
+              item.espacioAcademico,
+            ])
+          ).values()
+        ).filter((e): e is EspacioAcademico => e !== undefined);
+
+       
+        console.log('Programas cargados:', this.listProgramas);
+        console.log('Espacios cargados:', this.listEspacios);
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  filtrarEspaciosPorPrograma(idPrograma: number) {
+    this.espaciosFiltrados = this.listEspaciosPrograma
+      .filter((ep) => ep.programa?.idPrograma === idPrograma)
+      .map((ep) => ep.espacioAcademico!)
+      .filter(
+        (espacio, index, self) =>
+          index ===
+          self.findIndex(
+            (e) => e.idEspacioAcademico === espacio.idEspacioAcademico
+          )
+      );
+    console.log('Espacios filtrados:', this.espaciosFiltrados);
   }
   abrirCrearModal() {
     this.fg.reset();
@@ -168,69 +179,86 @@ export class GrupoRelacionComponent implements OnInit {
     this.displayCrearGrupo = true;
   }
 
-      // -- Metodo de crear -- //
-      creargrupoRelacion() {   
-        
-        /* const grupoId = this.fg?.get('grupo')?.value!; */
-        const sedeId = this.fg?.get('sede')?.value!;
-        const espacioId = this.fg?.get('espacio')?.value!;
-        const facultadId = this.fg?.get('facultad')?.value!;
-       
-        /* this.newGrupoRelacion.grupo = this.listGrupos.find(g => g.idGrupo === Number(grupoId))!; */
-        this.newGrupoRelacion.sede=this.listSedes.find(s => s.idSede === Number(sedeId))!;
-        if (this.newGrupoRelacion.espacioPrograma) {
-          this.newGrupoRelacion.espacioPrograma.espacioAcademico = this.listEspacios.find(e => e.idEspacioAcademico === Number(espacioId))!;
-        }
-        this.newGrupoRelacion.facultad=this.listFacultades.find(f => f.idFacultad === Number(facultadId))!;
-        this.newGrupoRelacion.idUsuarioCreacion = this.noDocumento;
-        if (this.fg.valid) {
-          this.grupoRelacionService.crearGrupoRelacion(this.newGrupoRelacion).subscribe({
-            next: (datagruporelacion) => {
-              this.messageService.add({
-                severity: 'success',
-                summary: 'CONFIRMACION',
-                detail: 'Registro creado con exito',
-              });
-              this.grupoRelacion = datagruporelacion;
-              this.listarGrupos();
-              this.cerrarCrearModal();
-            },
-            error: (dataerror) => {
-              this.messageService.add({
-                severity: 'error',
-                summary: 'ERROR',
-                detail: 'El registro ingresado ya existe'
-              });
+  // -- Metodo de crear -- //
+  creargrupoRelacion() {
+    const sedeId = this.fg?.get('sede')?.value!;
+    const facultadId = this.fg?.get('facultad')?.value!;
+    const idPrograma = this.fg?.get('programa')?.value!;
+    const idEspacio = this.fg?.get('espacio')?.value!;
+    
+
+    this.newGrupoRelacion.sede = this.listSedes.find(
+      (s) => s.idSede === Number(sedeId)
+    )!;
+    this.newGrupoRelacion.facultad = this.listFacultades.find(
+      (f) => f.idFacultad === Number(facultadId)
+    )!;
+
+    this.newGrupoRelacion.espacioPrograma=this.listEspaciosPrograma.find(
+      (ep) =>
+        ep.programa?.idPrograma === Number(idPrograma) &&
+        ep.espacioAcademico?.idEspacioAcademico === Number(idEspacio)
+    );
+
+
+      this.newGrupoRelacion.idUsuarioCreacion = this.noDocumento;  
+   
+
+    if (this.fg.valid) {
+      console.log('Objeto que se envía al backend:', this.newGrupoRelacion);
+      this.grupoRelacionService
+        .crearGrupoRelacion(this.newGrupoRelacion)
+        .subscribe({
+          next: (datagruporelacion) => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'CONFIRMACION',
+              detail: 'Registro creado con exito',
+            });
+            this.grupoRelacion = datagruporelacion;
+            this.listarGruposRelacion();
+            this.cerrarCrearModal();
+          },
+          error: (dataerror) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'ERROR',
+              detail: 'El registro ingresado ya existe',
+            });
+          }, 
+        });
+      this.displayCrearGrupo = false;
+    } else {
+      this.mensaje.mensajeError(
+        'Error al crear',
+        'Es necesario completar todos los campos del formulario para crear.'
+      );
+    }
+  }
+  eliminarGrupoRelacion(grupoRelacion: GrupoRelacion) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Esta acción eliminará permanentemente el grupo relacion. ¿Deseas continuar?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#26670f',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Si, eliminar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        console.log('El id es: ', grupoRelacion.idGrupoRelacion);
+
+        this.grupoRelacionService
+          .eliminarGrupoRelacion(grupoRelacion.idGrupoRelacion!)
+          .subscribe({
+            next: (datagrupo) => {
+              this.listarGruposRelacion();
             },
           });
-          this.displayCrearGrupo = false;
-        } else {
-          this.mensaje.mensajeError("Error al crear", "Es necesario completar todos los campos del formulario para crear.")
-        }
-    
       }
-     eliminarGrupoRelacion(grupoRelacion :GrupoRelacion){
-        Swal.fire({
-          title: "¿Estás seguro?",
-          text: "Esta acción eliminará permanentemente el grupo relacion. ¿Deseas continuar?",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#26670f",
-          cancelButtonColor: "#d33",
-          cancelButtonText: "Cancelar",
-          confirmButtonText: "Si, eliminar"
-        }).then((result) => {
-          if (result.isConfirmed) {
-            console.log("El id es: ", grupoRelacion.idGrupoRelacion);
-            
-            this.grupoService.eliminarGrupo(grupoRelacion.idGrupoRelacion!).subscribe({
-              next:(datagrupo)=>{
-                this.listarGruposRelacion();
-              }
-            });
-          }
-        });
-      }
+    });
+  }
 
   //----cerrar modales--//
   cerrarCrearModal(): void {
@@ -239,7 +267,5 @@ export class GrupoRelacionComponent implements OnInit {
 
   cerrarEditarModal(): void {
     this.displayEditarGrupo = false;
-
   }
-
 }
