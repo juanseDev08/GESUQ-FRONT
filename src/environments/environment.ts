81c9bbced80557bc90300e219cc8e35731eafbe2
dvalencia2 <dvalencia@vortexbird.com>
2025-09-25 10:37:34 -0500

    Eliminacion de codigo no usado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: false,
     urlbackend: "http://localhost:8080"
-    // urlbackend: "https://gpeuqtest.uniquindio.edu.co:8081/gpe"
-    // urlbackend: "http://10.0.58.77:8081"
 };
